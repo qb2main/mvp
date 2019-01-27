8b0fc7ddc6d990df91cfec9471c036ce589b7725
ii <qb2mail@gmail.com>
2019-01-27 19:00:09 +0200

    Use schema in admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Content;
 use App\Models\Footer;
 use App\Models\Header;
+use App\Models\Schema;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
@@ -12,9 +13,12 @@ use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
+    public $active_schema;
+
     public function __construct()
     {
         $this->middleware('auth');
+        $this->active_schema = Schema::where('active', true)->first();
     }
 
     /**
@@ -22,11 +26,11 @@ class AdminController extends Controller
      */
     public function index()
     {
-        $header = Header::first();
-        $content = Content::first();
-        $footer = Footer::first();
+        $schema = Schema::where('active', true)->with('header', 'content', 'footer')->first();
 
-        return view('admin.index', compact('header', 'content', 'footer'));
+        if (!$schema) echo "No schema exist";
+
+        return view('admin.index', compact('schema'));
     }
 
     /**
@@ -34,9 +38,13 @@ class AdminController extends Controller
      */
     public function headerForm()
     {
-        $header = Header::first();
-
-        return view('admin.parts.forms.header', compact('header'));
+        if (!$this->active_schema) {
+            echo "No active schema exist";
+        }
+        else {
+            $header = $this->active_schema->header;
+            return view('admin.parts.forms.header', compact('header'));
+        }
     }
 
     /**
@@ -47,7 +55,7 @@ class AdminController extends Controller
     {
         $header = Header::find($request->header_id);
 
-        if($header->update($request->all())) {
+        if ($header->update($request->all())) {
             return redirect()->back()->withSuccess('Header saved.');
         };
 
@@ -59,9 +67,13 @@ class AdminController extends Controller
      */
     public function contentForm()
     {
-        $content = Content::first();
-
-        return view('admin.parts.forms.content', compact('content'));
+        if (!$this->active_schema) {
+            echo "No active schema exist";
+        }
+        else {
+            $content = $this->active_schema->content;
+            return view('admin.parts.forms.content', compact('content'));
+        }
     }
 
     /**
@@ -72,7 +84,7 @@ class AdminController extends Controller
     {
         $content = Content::find($request->content_id);
 
-        if($content->update($request->all())) {
+        if ($content->update($request->all())) {
             return redirect()->back()->withSuccess('Content saved.');
         };
 
@@ -84,9 +96,13 @@ class AdminController extends Controller
      */
     public function footerForm()
     {
-        $footer = Footer::first();
-
-        return view('admin.parts.forms.footer', compact('footer'));
+        if (!$this->active_schema) {
+            echo "No active schema exist";
+        }
+        else {
+            $footer = $this->active_schema->content;
+            return view('admin.parts.forms.footer', compact('footer'));
+        }
     }
 
     /**
