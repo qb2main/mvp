9f0ad5e99122c054aea302dcd3576d360e3c2c5b
ii <qb2mail@gmail.com>
2019-01-27 02:09:25 +0200

    Show present image on admin

--- resources/views/admin/parts/forms/main.blade.php
+++ resources/views/admin/parts/forms/main.blade.php
@@ -4,24 +4,16 @@
     <hr>
     <h2>Main (image in header)</h2>
 
+    <img src="/images/header-bg.jpg" alt="Present image in header" height="360" width="620">
+
     <form action="{{ route('admin.image.save') }}" method="post" enctype="multipart/form-data">
         {{ csrf_field() }}
-
         <div class="form-group col-sm-12">
-            <label>Image upload</label>
+            <label>New image upload</label>
             <input  type="file" class="form-control" name="file">
         </div>
 
-
-
         <button type="submit" class="btn btn-success">Save</button>
-
     </form>
 
-@endsection
-
-@section('js')
-    <script>
-
-    </script>
 @endsection
